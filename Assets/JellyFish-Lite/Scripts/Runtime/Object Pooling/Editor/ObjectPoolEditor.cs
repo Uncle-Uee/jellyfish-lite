4da3c3e07b2f83a3d9fb50f870dfa3c38971fbec
Ubaidullah Effendi-Emjedi <mr.ueemjedi@gmail.com>
2020-05-07 22:00:55 +0200

    Added Object Pool Stripped out SOFlowEditor Library

---
+++ Assets/JellyFish-Lite/Scripts/Runtime/Object Pooling/Editor/ObjectPoolEditor.cs
@@ -0,0 +1,49 @@
+﻿// Created by Kearan Petersen : https://www.blumalice.wordpress.com | https://www.linkedin.com/in/kearan-petersen/
+
+#if UNITY_EDITOR
+using System.Collections;
+using System.Reflection;
+using UnityEditor;
+
+namespace JellyFish.ObjectPooling
+{
+    [CustomEditor(typeof(ObjectPoolBase), true)]
+    public class ObjectPoolEditor : UnityEditor.Editor
+    {
+        /// <summary>
+        ///     The ObjectPool target.
+        /// </summary>
+        private ObjectPoolBase _target;
+
+        private void OnEnable()
+        {
+            _target = (ObjectPoolBase) target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            IEnumerable pool = _target.GetPool();
+
+            foreach (object objectSet in pool)
+            {
+                EditorGUILayout.BeginHorizontal();
+
+                EditorGUILayout.LabelField("ID");
+
+                EditorGUILayout.LabelField($"Pool Count - {_target.CurrentPoolSize}");
+
+                EditorGUILayout.EndHorizontal();
+
+                PropertyInfo key   = objectSet.GetType().GetProperty("Key");
+                PropertyInfo value = objectSet.GetType().GetProperty("Value");
+
+                PropertyInfo valueCount = value.GetValue(objectSet).GetType().GetProperty("Count");
+                EditorGUILayout.LabelField(key.GetValue(objectSet).ToString());
+                EditorGUILayout.LabelField(valueCount.GetValue(value.GetValue(objectSet)).ToString());
+            }
+        }
+    }
+}
+#endif
---
+++ Assets/JellyFish-Lite/Scripts/Runtime/Object Pooling/Editor/PoolObjectListReferenceEditor.cs
@@ -0,0 +1,37 @@
+﻿// Created by Kearan Petersen : https://www.blumalice.wordpress.com | https://www.linkedin.com/in/kearan-petersen/
+
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+namespace JellyFish.ObjectPooling
+{
+    [CustomEditor(typeof(PoolObjectListReference))]
+    public class PoolObjectListReferenceEditor : UnityEditor.Editor
+    {
+        /// <summary>
+        /// The PoolObjectListReference target.
+        /// </summary>
+        public PoolObjectListReference _target;
+
+        public void OnEnable()
+        {
+            _target = (PoolObjectListReference) target;
+        }
+
+        /// <inheritdoc />
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            EditorGUILayout.LabelField("Total Pool Objects");
+
+            EditorGUILayout.LabelField(_target.PoolObjectCount.ToString());
+            foreach (IPoolObjectRoot poolObject in _target.PoolObjects)
+            {
+                EditorGUILayout.ObjectField(poolObject.GetObjectInstance(), typeof(Object), false);
+            }
+        }
+    }
+}
+#endif
