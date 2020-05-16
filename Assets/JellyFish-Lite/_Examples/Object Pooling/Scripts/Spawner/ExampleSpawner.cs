9b4e29cb532ef89d840f57237c0a3c5db1061770
Ubaidullah Effendi-Emjedi <mr.ueemjedi@gmail.com>
2020-05-16 15:11:04 +0200

    Updated Object Pool Example Scene

---
+++ Assets/JellyFish-Lite/_Examples/Object Pooling/Scripts/Spawner/ExampleSpawner.cs
@@ -0,0 +1,28 @@
+﻿using Examples.ObjectPool.SOFlow.Examples;
+using UnityEngine;
+
+namespace Examples.ObjectPool
+{
+    public class ExampleSpawner : MonoBehaviour
+    {
+        #region VARIABLES
+
+        [Header("Object Pool Reference")]
+        public ExampleObjectPool ObjectPool;
+
+        private ExamplePoolObject _poolObject;
+
+        #endregion
+
+        #region METHODS
+
+        public void SpawnObject(string id)
+        {
+            _poolObject                    = ObjectPool.GetObjectFromPool(id, false);
+            _poolObject.transform.position = transform.position;
+            _poolObject.gameObject.SetActive(true);
+        }
+
+        #endregion
+    }
+}
