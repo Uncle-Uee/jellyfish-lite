17a5b543d6c19a31c308d744c0f83f8d1f37f26f
Ubaidullah Effendi-Emjedi <mr.ueemjedi@gmail.com>
2020-07-09 12:57:27 +0200

    Added Object Pool

---
+++ Assets/JellyFish-Lite/_Examples/Object Pooling/Scripts/ObjectPool/ExampleObjectPool.cs
@@ -0,0 +1,15 @@
+﻿// Created by Kearan Petersen : https://www.blumalice.wordpress.com | https://www.linkedin.com/in/kearan-petersen/
+
+using SOFlow.ObjectPooling;
+using UnityEngine;
+
+namespace Examples.ObjectPool
+{
+    namespace SOFlow.Examples
+    {
+        [CreateAssetMenu(menuName = "JellyFish/Examples/Example Object Pool")]
+        public class ExampleObjectPool : ObjectPool<ExamplePoolObject>
+        {
+        }
+    }
+}
